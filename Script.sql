1f453107c7b3715d32591938ad674bf1c2e929be
jeremydassaud <jerem.dassaud63@gmail.com>
2024-01-29 15:03:38 +0100

    suppression table Departements

---
+++ Script.sql
@@ -0,0 +1,12 @@
+PRAGMA table_info(users);
+PRAGMA table_info(Plante);
+PRAGMA table_info(commentaire);
+PRAGMA table_info(Aide);
+PRAGMA table_info(Asso);
+
+SELECT * FROM users;
+SELECT * FROM Plante;
+SELECT * FROM Aide;
+SELECT * FROM Asso;
+
+DROP TABLE IF EXISTS Departments;
