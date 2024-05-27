9729504db27c6a5c4b088dbf0c97dc4053814241
GuillaumeBONNAMOUR <97189441+GuillaumeBONNAMOUR@users.noreply.github.com>
2024-05-27 14:58:07 +0200

    correction erreur apostrophe CookieBanner.tsx (#17)
    Co-authored-by: WilliamGoodlove <guillaumebonnamour38300@gmail.com>

--- components/atomicDesign/mollecules/banner/CookieBanner.tsx
+++ components/atomicDesign/mollecules/banner/CookieBanner.tsx
@@ -22,7 +22,7 @@ const CookieBanner = () => {
         <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-4/5 max-w-3xl p-4 bg-gray-800 bg-opacity-90 backdrop-blur-sm text-white rounded-lg shadow-lg flex flex-col justify-between text-center">
             <span>
                 En acceptant les cookies, je souhaite améliorer mon expérience sur ce site et recevoir des
-                contenus personnalisés. Pour plus d'informations, consultez notre politique de cookies.
+                contenus personnalisés. Pour plus d&apos;informations, consultez notre politique de cookies.
             </span>
             <div className="flex justify-end mt-4">
                 <button onClick={handleAccept} className="bg-green-500 p-2 mx-2 rounded">Accepter</button>
